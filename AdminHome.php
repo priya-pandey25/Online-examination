7464194d3413fd1924c14e9e52aec239bfbdb46b
Priya Pandey <151717956+priya-pandey25@users.noreply.github.com>
2025-01-18 13:47:32 +0530

    Add files via upload

---
+++ AdminHome.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@800&family=Baloo+Bhaina+2:wght@800&display=swap" rel="stylesheet">
+    <style>
+        /* CSS RESET  */
+        body{
+            font-family: 'Baloo Bhai 2', cursive;
+            margin: 0px;
+            padding: 0px;
+            background: url(Images/Student7.jpg);
+            background-repeat:no-repeat ;
+           background-size: 1550px 800px;  
+           font-family: Baloo Bhai;  
+           
+        }
+        .navbar
+        {
+         display: inline-block;
+         /* border: 3px solid white; */
+        margin-left: 2%;
+         margin-top: 25px;
+         border-radius: 5px;
+        }
+        .navbar li{
+            display: inline-block;
+        }
+        .navbar li a{
+            color: white;
+            font-size: 23px;
+            padding: 13px 15px;
+            text-decoration: none;
+        }
+        .navbar li a:hover{
+           
+           color: grey;
+           font-size: 23px;
+           padding: 13px 15px;
+           text-decoration: none; 
+       }
+     
+
+    
+    </style>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+        <ul>
+        <li><a href="StudentList.php">Student List</a> </li>
+        <li><a href="AddQuestion.php">Add Question</a></li>
+        <li><a href="UpdateQuestion.php">Update Question</a></li>
+        <li> <a href="QuestionList.php">Question List</a></li>
+        <li><a href="ExamResult.php">Result</a></li>
+        <li><a href="Feedback.php">Feedback</a></li>
+        <li><a href="ChangePassword.php">Account</a></li>
+        <li><a href="Logout.php">Logout</a></li>
+        </ul>
+    </div><hr>
+    </header>
+    
+</body>
+</html>
